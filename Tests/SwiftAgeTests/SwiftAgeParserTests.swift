bd1c249d259d7f4200aff7027425090badad7afb
Joshua Jacob <josh.jacob@joshjacob.com>
2023-10-16 16:35:21 -0400

    Swift tests on Linux

--- Tests/SwiftAgeTests/SwiftAgeParserTests.swift
+++ Tests/SwiftAgeTests/SwiftAgeParserTests.swift
@@ -1,6 +1,10 @@
 import XCTest
 @testable import SwiftAge
 
+#if os(Linux)
+    import let CDispatch.NSEC_PER_SEC
+#endif
+
 final class SwiftAgeParserTests: XCTestCase {
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
